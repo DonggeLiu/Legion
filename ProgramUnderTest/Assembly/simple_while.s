518317d9a8699de57173ae391ceddc41d3a54236
Alan32Liu <alan32.liu@gmail.com>
2018-12-05 12:12:50 +1100

    assmbly code and related

---
+++ ProgramUnderTest/Assembly/simple_while.s
@@ -0,0 +1,67 @@
+	.file	"simple_while.c"
+	.text
+	.globl	test
+	.type	test, @function
+test:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	jmp	.L2
+.L3:
+	subl	$1, -4(%rbp)
+.L2:
+	cmpl	$110, -4(%rbp)
+	ja	.L3
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	test, .-test
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	-13(%rbp), %rax
+	movl	$1, %edx
+	movq	%rax, %rsi
+	movl	$0, %edi
+	call	read@PLT
+	movzbl	-13(%rbp), %eax
+	movzbl	%al, %eax
+	movl	%eax, %edi
+	call	test
+	movl	%eax, -12(%rbp)
+	movzbl	-13(%rbp), %eax
+	movzbl	%al, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L7
+	call	__stack_chk_fail@PLT
+.L7:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits

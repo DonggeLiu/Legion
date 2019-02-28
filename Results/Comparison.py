672d3ab0ec8830ede95f4f9ec5c154a3ea1e70bf
Dongge Liu <alan32.liu@gmail.com>
2019-02-28 14:21:45 +1100

    Automatic performance comparisons

--- Results/Comparison.py
+++ Results/Comparison.py
@@ -1,28 +1,25 @@
-import subprocess as sp
 import csv
+import subprocess as sp
 
-cut_off = 500 # only for csv plotting
-repitition = 50 # trials
+cut_off = 500  # only for csv plotting
+repitition = 50  # trials
 max_path = 7
 
 command = "python fuzz_new_alternative.py ./test_wide.instr"
 c_list = command.split()
-# print(c_list)
 
 sum_data = [0] * cut_off
 for _ in range(repitition):
-	histogram = sp.check_output(c_list + [' '])
-	data = [line.split(b',')[1] for line in histogram.splitlines() if line]
-	data += [max_path] * (len(sum_data) - len(data))
-	for i in range(len(sum_data)):
-		sum_data[i] += float(data[i])
-	# print(data)
-	# print(sum_data)
+    histogram = sp.check_output(c_list + [' '])
+    data = [line.split(b',')[1] for line in histogram.splitlines() if line]
+    data += [max_path] * (len(sum_data) - len(data))
+    for i in range(len(sum_data)):
+        sum_data[i] += float(data[i])
 
 
 average_data1 = []
 for i in sum_data:
-	average_data1.append(i/repitition)
+    average_data1.append(i / repitition)
 
 # print(average_data)
 # plot(average_data)
@@ -35,20 +32,16 @@ c_list.append(' ')
 
 sum_data = [0] * cut_off
 for _ in range(repitition):
-	# print(c_list)
-	histogram = sp.check_output(c_list)
-	# print(histogram)
-	data = [line.split(b',')[1] for line in histogram.splitlines() if line]
-	data += [max_path] * (len(sum_data) - len(data))
-	for i in range(len(sum_data)):
-		sum_data[i] += float(data[i])
-	# print(data)
-	# print(sum_data)
+    histogram = sp.check_output(c_list)
+    data = [line.split(b',')[1] for line in histogram.splitlines() if line]
+    data += [max_path] * (len(sum_data) - len(data))
+    for i in range(len(sum_data)):
+        sum_data[i] += float(data[i])
 
 
 average_data2 = []
 for i in sum_data:
-	average_data2.append(i/repitition)
+    average_data2.append(i / repitition)
 
 # print(average_data2)
 # plot(average_data)
@@ -68,43 +61,25 @@ c_list.append(' ')
 
 sum_data = [0] * cut_off
 for _ in range(repitition):
-	# print(c_list)
-	histogram = sp.check_output(c_list)
-	# print(histogram)
-	data = [line.split(b',')[1] for line in histogram.splitlines() if line]
-	data += [max_path] * (len(sum_data) - len(data))
-	for i in range(len(sum_data)):
-		sum_data[i] += float(data[i])
-	# print(data)
-	# print(sum_data)
+    histogram = sp.check_output(c_list)
+    data = [line.split(b',')[1] for line in histogram.splitlines() if line]
+    data += [max_path] * (len(sum_data) - len(data))
+    for i in range(len(sum_data)):
+        sum_data[i] += float(data[i])
 
 
 average_data3 = []
 for i in sum_data:
-	average_data3.append(i/repitition)
+    average_data3.append(i / repitition)
 
 # print(average_data)
 # plot(average_data)
 
 
 with open('results.csv', 'wb') as csvfile:
-	result_writer = csv.writer(csvfile, delimiter = ',',
-		quotechar="|", quoting=csv.QUOTE_MINIMAL)
-	result_writer.writerow(['Iteration', 'Equal', 'Different', 'Random'])
-	for i in range(len(average_data1)):
-		result_writer.writerow(
-			[i+1, average_data1[i], average_data2[i], average_data3[i]])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    result_writer = csv.writer(
+        csvfile, delimiter=',', quotechar="|", quoting=csv.QUOTE_MINIMAL)
+    result_writer.writerow(['Iteration', 'Equal', 'Different', 'Random'])
+    for i in range(len(average_data1)):
+        result_writer.writerow(
+            [i + 1, average_data1[i], average_data2[i], average_data3[i]])
--- Results/statisticQS.py
+++ Results/statisticQS.py
@@ -1,63 +1,82 @@
-import subprocess as sp
-# import tkinter
-from matplotlib.pyplot import plot
 import csv
+import subprocess as sp
+import sys
 import time
 import traceback
-import sys
 
+repetition = 100  # trials
+SEED = ''.join(sys.argv[1:])
 
-repitition = 50 # trials
 
 def average(nums):
 	return sum(nums)/len(nums)
+
 
 def variance(nums):
 	avg = average(nums)
 	return sum([(num-avg)**2 for num in nums])
 
+
 def print_statics(nums):
-	print(nums, average(nums), variance(nums))
+	iter_count = []
+	time_count = []
+	for num in nums:
+		iter_count.append(num[0])
+		time_count.append(num[1])
+	print(
+		nums, average(iter_count), variance(iter_count), average(time_count), variance(time_count))
 
-def compute_historgram(command):
+
+def compute_histogram(command):
 	c_list = command.split()
-
+	print(command)
 	histogram = []
-	for _ in range(repitition):
-		history = sp.check_output(c_list + ['s'])
+	for _ in range(repetition):
+		ts = time.time()
+		history = sp.check_output(c_list)
+		te = time.time()
 		cost = int(history.splitlines()[-1].split(b',')[0])
-		histogram.append(cost)
-
+		histogram.append((cost, te - ts))
 	return histogram
-try:
-	command = "python fuzz_qs.py PUT/SourceCode/test_half "
-	histogram1 = compute_historgram(command)
-	print_statics(histogram1)
-except:
-	traceback.print_exc(file=sys.stdout)
+
+
+# try:
+# 	qs_command = \
+# 		"python3 fuzz_qs.py ProgramUnderTest/Instrumented/test_half.instr " + SEED[0]
+# 	histogram1 = compute_histogram(qs_command)
+# 	print_statics(histogram1)
+# except KeyboardInterrupt:
+# 	traceback.print_exc(file=sys.stdout)
+# 	histogram1 = [0] * repetition
 
 try:
-	command = "python fuzz_legion.py PUT/SourceCode/test_half "
-	histogram2 = compute_historgram(command)
+	pc_command = \
+		"python3 principes.py ProgramUnderTest/Instrumented/simple_while.instr " + SEED
+	histogram2 = compute_histogram(pc_command)
 	print_statics(histogram2)
-except:
+except KeyboardInterrupt:
 	traceback.print_exc(file=sys.stdout)
+	histogram2 = [0] * repetition
 
-try:	
-	command = "python fuzz_random.py PUT/SourceCode/test_half "
-	histogram3 = compute_historgram(command)
+try:
+	rd_command = \
+		"python3 Benchmarks/pure_random.py ProgramUnderTest/Instrumented/simple_while.instr " + SEED
+	histogram3 = compute_histogram(rd_command)
 	print_statics(histogram3)
-except:
+except KeyboardInterrupt:
 	traceback.print_exc(file=sys.stdout)
-
+	histogram3 = [0] * repetition
 
 
 with open(
-	'QS_test_half_50_{}.csv'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())), 
-	'wb') as histogram_file:
-	histo_writer = csv.writer(histogram_file, delimiter = ',', 
-		quotechar="|", quoting=csv.QUOTE_MINIMAL)
-	histo_writer.writerow(['', 'QS', 'Prev', 'Random' ])
-	for i in range(repitition):
-		histo_writer.writerow([i, histogram1[i], histogram2[i], histogram3[i]])
-
+		'QS_simple_while_100_{}.csv'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())), 'wb') \
+		as histogram_file:
+	histo_writer = csv.writer(
+		histogram_file, delimiter=',')
+	histo_writer.writerow(['', 'Current', '', 'Random'])
+	for i in range(repetition):
+		histo_writer.writerow(
+			[i,
+			 # histogram1[i][0], histogram1[i][1],
+			 histogram2[i][0], histogram2[i][1],
+			 histogram3[i][0], histogram3[i][1]])

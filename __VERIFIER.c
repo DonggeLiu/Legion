05dbbf761bdd5d63ead2ac6d4c2aa6724f064040
Alan32Liu <alan32.liu@gmail.com>
2019-10-03 13:19:45 +1000

    unsigned long should be printed with lu

--- __VERIFIER.c
+++ __VERIFIER.c
@@ -45,7 +45,7 @@ unsigned short __VERIFIER_nondet_ushort() {
 unsigned long __VERIFIER_nondet_unsigned_long() {
     unsigned long x = 0;
     read(0, &x, sizeof(x));
-    printf("  <input type=\"unsigned long\">%uld</input>\n", x);
+    printf("  <input type=\"unsigned long\">%lu</input>\n", x);
     return x;
 }
 

4d0a86399e3071dcf42cfb6f0cddfd160d84d16b
Dongge Liu <alan32.liu@gmail.com>
2019-11-07 14:29:40 +1100

    Use the correct assume and avoid it being rewritten to the un-instrumented version

--- Legion.py
+++ Legion.py
@@ -1214,7 +1214,7 @@ if __name__ == '__main__':
             sp.run([args.cc, "-no-pie", "-o", asm, "-S", source])
             sp.run(["./tracejump.py", asm, ins])
             sp.run([args.cc, "-no-pie", "-O0", "-o", BINARY, "__VERIFIER.c",
-                    "__VERIFIER_assume.s",
+                    "__VERIFIER_assume.instr.s",
                     "__trace_jump.s", ins])
         elif args.compile == "trace-cc":
             if args.o:

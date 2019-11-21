4a59b9543c0eeb1c727f2979d306498a7b17bdf2
Alan32Liu <alan32.liu@gmail.com>
2019-11-21 22:03:02 +1100

    test time consumption on large input

--- Testing/TraceJumpTests/test_while.c
+++ Testing/TraceJumpTests/test_while.c
@@ -1,17 +1,23 @@
-extern int __VERIFIER_nondet_int(void);
+#include <stdio.h>
+
+extern int __VERIFIER_nondet_uint(void);
 
 
 unsigned int test(int x) {
 	while (x)
 	{
 		x --;
+//		fwrite(&x, 8, 1, stderr);
 	}
 	return 0;
 }
 
 
 int main(int argc, char * argv[]) {
-	int x = __VERIFIER_nondet_int();
+//    printf("%d", BUFSIZ);
+//    char buf[BUFSIZ];
+//    setbuf(stderr, buf);
+	unsigned int x = __VERIFIER_nondet_uint();
 	test(x);
 	return 0;
 }
